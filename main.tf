708cdfb2247ce8d4e2fe2defcfa334eac0e9c991
PARAG PATIL <109343892+paragpatil01@users.noreply.github.com>
2023-10-30 16:43:59 +0530

    Create main.tf
    creating aws instance using terrafrom

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+
+  region     = "ap-south-1"
+}
+
+resource "aws_instance" "Demo" {
+  ami           = "ami-0d758c1134823146a"
+  instance_type = "t2.micro"
+  key_name = "myawslogin"
+  tags = {
+    Name = "Demo"
+  }
+}
